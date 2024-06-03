6b4e6eecb75011cc734ceb8ccf8b7fcf886dbb85
Anhthu02tranthi <thuttade160388@fpt.edu.vn>
2024-06-03 17:03:22 +0700

    Node MongoDB Example 2

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -3,12 +3,15 @@ const assert = require('assert');
 
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
+const dboper = require('./operations');
+
 
 MongoClient.connect(url, (err, client) => {
 
 assert.equal(err,null);
 
 console.log('Connected correctly to server');
+
 
 const db = client.db(dbname);
 const collection = db.collection("dishes");
@@ -28,7 +31,30 @@ console.log(docs);
 db.dropCollection("dishes", (err, result) => {
 assert.equal(err,null);
 
-client.close();
+dboper.insertDocument(db, { name: "Vadonut", description: "Test"},
+"dishes", (result) => {
+    console.log("Insert Document:\n", result.ops);
+
+    dboper.findDocuments(db, "dishes", (docs) => {
+        console.log("Found Documents:\n", docs);
+
+        dboper.updateDocument(db, { name: "Vadonut" },
+            { description: "Updated Test" }, "dishes",
+            (result) => {
+                console.log("Updated Document:\n", result.result);
+
+                dboper.findDocuments(db, "dishes", (docs) => {
+                    console.log("Found Updated Documents:\n", docs);
+                    
+                    db.dropCollection("dishes", (result) => {
+                        console.log("Dropped Collection: ", result);
+
+                        client.close();
+                    });
+                });
+            });
+    });
+});
 });
 });
 });
